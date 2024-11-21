0fdb82c705cab1b3ec0a4a2bfe733bc59e5527e9
Wahab901278 <abdulwahab901278@gmail.com>
2024-11-21 20:04:20 +0500

    increased logo size and added gmail in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,12 @@ const Footer = () => {
   return (
     <footer className="bg-light py-4 text-center">
       <div className="container">
+        <div className="contact">
+          <p>
+            <b style={{ margin: "2px" }}>Contact : </b>
+          </p>
+          <p>mammoguard@gmail.com</p>
+        </div>
         <p>&copy; MammoGuard. All rights reserved.</p>
       </div>
     </footer>
